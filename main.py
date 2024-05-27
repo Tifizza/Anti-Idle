f6327fe4ea3b8fcacb55d6d8380a68f9a3d9c3c8
Tifizza <10532741+Tifizza@users.noreply.github.com>
2024-05-27 11:42:43 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import win32api
+import time
+import pyautogui as pg
+IDLE_TIME = 5
+
+
+def get_idle_time():
+    return (win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0
+
+
+def is_idle():
+    return get_idle_time() > IDLE_TIME
+
+
+while True:
+    if is_idle():
+        pg.typewrite(['ctrl'])
+    time.sleep(IDLE_TIME-2)
